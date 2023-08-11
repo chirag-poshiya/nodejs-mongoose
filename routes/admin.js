16dcbf0cdc598eb26c26421b2062678765af5e32
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-08-11 20:26:11 +0530

    update

--- routes/admin.js
+++ routes/admin.js
@@ -9,11 +9,12 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
-// /admin/products => GET
-router.get('/products', adminController.getProducts);
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
-// // /admin/add-product => POST
-// router.post('/add-product', adminController.postAddProduct);
+// // /admin/products => GET
+// router.get('/products', adminController.getProducts);
+
 
 // router.get('/edit-product/:productId', adminController.getEditProduct);
 
