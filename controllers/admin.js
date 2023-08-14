a11339158a9b69cd9da9508cfb5c593236143349
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-08-14 11:19:24 +0530

    update login

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,7 +5,8 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false
+    editing: false,
+    isAuthenticated: req.isLoggedIn
   });
 };
 
@@ -19,7 +20,8 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     imageUrl: imageUrl, 
     description: description,
-    userId: req.user
+    userId: req.user,
+    isAuthenticated: req.isLoggedIn
   });
   product
     .save()
@@ -47,7 +49,8 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing: editMode,
-        product: product
+        product: product,
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -83,7 +86,8 @@ exports.getProducts = (req, res, next) => {
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
-        path: '/admin/products'
+        path: '/admin/products',
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
