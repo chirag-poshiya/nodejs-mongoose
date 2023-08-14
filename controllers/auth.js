2d143126627e9777b48f63a0fc80eef1bf424614
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-08-14 11:40:30 +0530

    update login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,13 @@
 exports.getLogin = (req, res, next) => {
-    res.render('auth/login', {
-        path: '/login',
-        pageTitle: 'Login',
-        isAuthenticated: req.isLoggedIn
-        });  
+  const isLoggedIn = (req.get("Cookie").split(";")[1].trim().split("=")[1] == 'true');
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn,
+  });
 };
 
-
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
-    res.redirect('/');
-}
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.redirect("/");
+};
