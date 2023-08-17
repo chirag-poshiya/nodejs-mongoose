4af72824f865daaa5b8744871756469fc8bc35ca
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-08-17 10:28:32 +0530

    update login

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const session = require('express-session')
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = "mongodb://localhost:27017/shop";
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI, 
+  collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,7 +26,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secret', resave: false, saveUninitialized: false}))
+app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}))
 
 app.use((req, res, next) => {
   User.findById('64d706f646338d0b9848c05b')
@@ -38,7 +45,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb://localhost:27017/shop', {useNewUrlParser: true, useUnifiedTopology: true}
+    MONGODB_URI , {useNewUrlParser: true, useUnifiedTopology: true}
   )
   .then(result => {
     User.findOne().then(user => {
