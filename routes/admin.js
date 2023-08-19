b310304f9a58f766ed1ebd6961a8539aff7a8c24
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-08-19 16:34:34 +0530

    update

--- routes/admin.js
+++ routes/admin.js
@@ -12,13 +12,16 @@ router.get('/add-product', adminController.getAddProduct);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-// // /admin/products => GET
+// /admin/products => GET
 router.get('/products', adminController.getProducts);
 
+// /admin/edit-product/productId => GET, To get one product details for editing
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
+// /admin/edit-product => POST, To edit one product details
 router.post('/edit-product', adminController.postEditProduct);
 
+// /admin/delete-product => POST, To delete one product
 router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
