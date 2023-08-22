9dc7c083a24e81d3103e4d5924cdd8e63cab9f48
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-08-22 10:01:18 +0530

    update session

--- app.js
+++ app.js
@@ -58,18 +58,6 @@ mongoose
     MONGODB_URI , {useNewUrlParser: true, useUnifiedTopology: true}
   )
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(err => {
