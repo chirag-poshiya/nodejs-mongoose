e2d6c23353c3dc0143c55a017f75cb825dbc26ff
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-08-22 09:41:16 +0530

    update session

--- routes/admin.js
+++ routes/admin.js
@@ -9,19 +9,16 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
-// /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct);
-
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
 
-// /admin/edit-product/productId => GET, To get one product details for editing
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
-// /admin/edit-product => POST, To edit one product details
 router.post('/edit-product', adminController.postEditProduct);
 
-// /admin/delete-product => POST, To delete one product
 router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
