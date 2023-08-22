fb6c8c3abcf1b005564d17091c64d30247be931e
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-08-22 10:33:32 +0530

    update session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const user = require('../models/user');
+const bcrypt = require('bcryptjs')
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -18,16 +18,39 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  User.findOne({email:email})
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if(!user){
+        return res.redirect('/login')
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if(doMatch){
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log('error session save');
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+            return res.redirect('/login');
+          
+        })
+        .catch(err => {
+          console.log('err bcrypt')
+          console.log(err)
+        });
+      
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log('error find user')
+      console.log(err)
+    });
 };
 
 exports.postSignup = (req, res, next) => {
@@ -40,16 +63,21 @@ exports.postSignup = (req, res, next) => {
     if(userDoc){
       return res.redirect('/signup');
     }
-    const user = new User({
-      email: email,
-      password: password,
-      cart:{ items: [] }
-    });
-    return user.save();
+    return bcrypt.hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart:{ items: [] }
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login')
+    })
+    
   })
-  .then(result => {
-    res.redirect('/login')
-  })
+  
   .catch(err => {
     console.log(err)
   });
